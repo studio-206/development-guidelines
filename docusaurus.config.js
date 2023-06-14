eed348848c3eec5d0ce8b6e5fcef06379fbc59a3
Pedro Valentim <pedro@vlt.im>
2023-06-15 00:19:48 +0100

    Fix social card

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -55,7 +55,7 @@ const config = {
     /** @type {import('@docusaurus/preset-classic').ThemeConfig} */
     {
       // Replace with your project's social card
-      image: "img/social-card.jpg",
+      image: "img/social-card.png",
       colorMode: {
         defaultMode: "dark",
         disableSwitch: true,
